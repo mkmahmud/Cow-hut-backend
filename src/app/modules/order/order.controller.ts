56caee264e5f35d1f8bd7c625ccec99127425072
mkmahmud <mahmudulhasanmk434@gmail.com>
2023-06-17 21:20:56 +0600

    [DONE] handel buyer budget and cow price

--- src/app/modules/order/order.controller.ts
+++ src/app/modules/order/order.controller.ts
@@ -1,17 +1,33 @@
 import { NextFunction, Request, Response } from 'express'
 import { orderService } from './order.service'
+import { Cow } from '../cows/cow.model'
+import { User } from '../user/user.model'
 
 const createOrder = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const data = req.body
-    const result = await orderService.createNewOrder(data)
 
-    res.status(200).json({
-      statusCode: 200,
-      success: true,
-      message: 'Order made successfully',
-      data: result,
-    })
+    // Get cow details
+    const cowDetails = await Cow.findById(data.cow)
+    console.log(cowDetails)
+    const buyerDetails = await User.findById(data.buyer)
+    console.log(buyerDetails)
+
+    if (buyerDetails && cowDetails && buyerDetails.budget >= cowDetails.price) {
+      const result = await orderService.createNewOrder(data)
+      res.status(200).json({
+        statusCode: 200,
+        success: true,
+        message: 'Order made successfully',
+        data: result,
+      })
+    } else {
+      res.status(404).json({
+        statusCode: 404,
+        success: false,
+        message: 'Your dont have enough money',
+      })
+    }
   } catch (error) {
     next(error)
   }
